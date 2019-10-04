d690c70565f11aeb0bcb259d2555c681bd277223
Djavan Munroe <djavanm24@gmail.com>
2019-10-04 09:48:33 -0600

    Move listen on server

--- server.js
+++ server.js
@@ -7,25 +7,22 @@ const database = require('knex')(configuration);
 app.use(express.json());
 app.set('port', process.env.PORT || 3000).
 
-app.listen(app.get('port'), () => {
-  console.log(`App is listening on port ${app.get('port')}`);
-});
 
 app.get('/', (req, res)=> {
   res.send('<p>Now, this is a story all about how</p>'
-    + '<p>My life got flipped-turned upside down</p>'
-    + '<p>And I\'d like to take a minute Just sit right there</p>'
-    + '<p>I\'ll tell you how I became the prince of a town called Bel Air...</p>');
+  + '<p>My life got flipped-turned upside down</p>'
+  + '<p>And I\'d like to take a minute Just sit right there</p>'
+  + '<p>I\'ll tell you how I became the prince of a town called Bel Air...</p>');
 });
 
 app.get('/api/v1/sitcoms', (request, response) => {
   database('sitcoms').select()
-    .then((sitcoms) => {
-      response.status(200).json(sitcoms);
-    })
-    .catch((error) => {
-      response.status(500).json({ error });
-    });
+  .then((sitcoms) => {
+    response.status(200).json(sitcoms);
+  })
+  .catch((error) => {
+    response.status(500).json({ error });
+  });
 });
 
 app.get('/api/v1/castMembers', (request, response) => {
@@ -36,4 +33,8 @@ app.get('/api/v1/castMembers', (request, response) => {
     .catch((error) => {
       response.status(500).json({ error });
     });
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`App is listening on port ${app.get('port')}`);
 });
